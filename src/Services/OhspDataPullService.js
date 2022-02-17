07670efc88c04118dc96b8e5ba050ac5b31f7bca
Dominic Kasanga <dominickasanga@gmail.com>
2022-02-17 23:28:03 +0200

    successfully pulled data from OHSP test server

---
+++ src/Services/OhspDataPullService.js
@@ -0,0 +1,32 @@
+const  https = require ('https')
+
+function pullData() {
+    var user = "eidsr"
+    var password = "Eidsr2021!"
+    var auth = "Basic " + new Buffer(user + ":" + password).toString("base64")
+
+    const req = https.request(
+        {
+            host: 'covmw.com',
+            path: '/ohspdev/api/dataValueSets.json?dataSet=waoQ016uOz1&period=2022W6&orgUnit=XcRAuC6ZPi0',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization' : auth,
+                'Accept' : '*/*'
+            }
+        },
+        response => {
+            var str = "";
+            response.on('data', (d) => {
+                str+=d;
+            }).on('end',()=>{
+                console.log(str);
+            });
+        }
+    )
+     
+    req.end();
+}
+
+pullData()
