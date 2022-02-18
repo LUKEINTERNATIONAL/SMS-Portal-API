e3eeec5c07a895e9e35aa074160b107ee9acdd6a
Dominic Kasanga <dominickasanga@gmail.com>
2022-02-18 13:11:20 +0200

    mapping weekly

--- src/Services/OhspInterface.js
+++ src/Services/OhspInterface.js
@@ -1,6 +1,7 @@
 const csv = require('csv-parser')
 const fs = require('fs')
 const results = []
+const Weekly = []
 const  https = require ('https')
 
 /* reading Health Facilities.csv */
@@ -14,44 +15,80 @@ fs.createReadStream('../IDSRMetadata/Health Facilities.csv')
       map(results)
   })
 
-  async function map(csv_Obj) {
-    var user = "eidsr"
-    var password = "Eidsr2021!"
-    var auth = "Basic " + new Buffer(user + ":" + password).toString("base64")
+async function map(csv_Obj) {
+  var user = "eidsr"
+  var password = "Eidsr2021!"
+  var auth = "Basic " + new Buffer(user + ":" + password).toString("base64")
 
-    const req = https.request(
-        {
-            host: 'covmw.com',
-            path: '/ohspdev/api/dataValueSets.json?dataSet=waoQ016uOz1&period=2022W6&orgUnit=XcRAuC6ZPi0',
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization' : auth,
-                'Accept' : '*/*'
-            }
-        },
-        response => {
-            var data = "";
-            response.on('data', (d) => {
-                data+=d;
-            }).on('end',()=>{
-              
-              data = JSON.parse(data)
-              console.log(data.orgUnit)
-              for (let facility of csv_Obj) {
-              let OrgUnitID = facility.OrgUnitID
-              let OrgUnitName = facility.OrgUnitName
-              if (OrgUnitID == data.orgUnit) {
-                console.log(OrgUnitName)
-              }
-                // for (let set in data) {
-                //   //console.log(typeof(set))
-                //   //console.log("orgUnit: ", set.dataSet)
-                // }
-              }
-            });
+  const req = https.request(
+    {
+      host: 'covmw.com',
+      path: '/ohspdev/api/dataValueSets.json?dataSet=waoQ016uOz1&period=2022W6&orgUnit=XcRAuC6ZPi0',
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization' : auth,
+          'Accept' : '*/*'
+      }
+    },
+    response => {
+      var data = ""
+      response.on('data', (d) => {
+          data+=d;
+      }).on('end', () => {
+        data = JSON.parse(data)
+        console.log(data.orgUnit)
+
+        for (let facility of csv_Obj) {
+          let OrgUnitID = facility.OrgUnitID
+          let OrgUnitName = facility.OrgUnitName
+
+          if (OrgUnitID == data.orgUnit) {
+            console.log(OrgUnitName)
+            mapCase('Weekly', data.dataValues)
+          }
         }
-    )
-     
-    req.end()
-  }
+      })
+
+    }
+  )
+
+  req.end();
+}
+
+function mapCase(type, dataValues) {
+  let file = 'IDSR '+ type +' Reporting Integration IDs.csv'
+  fs.createReadStream('../IDSRMetadata/'+file)
+  .pipe(csv({}))
+  .on('data', (data) => {
+    Weekly.push(data)
+  })
+  .on('end', () => {
+      console.log(Weekly)
+      for (let case_ of Weekly) {
+        let DataSetID = case_.DataSetID
+        let DataElementName = case_.DataElementName
+        let DataElementUID = case_.DataElementUID
+        let OptionLessFive = case_.OptionLessFive
+        let OptionGreaterEqualFive = case_.OptionGreaterEqualFive
+
+        for (let condition of dataValues) {
+
+          if (DataElementUID == condition.dataElement) {
+            console.log(DataElementName)
+          } 
+            
+          
+          if (OptionLessFive == condition.categoryOptionCombo) {
+            console.log("here")
+          }
+
+          if (OptionGreaterEqualFive == condition.attributeOptionCombo) {
+            console.log("ereh")
+          }
+              
+            
+        }
+      }
+  })
+}
