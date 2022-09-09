244b82d2e437bc9c7baa984e764b056bc78cbac0
dominickasanga <dominickasanga@gmail.com>
2022-09-09 11:14:10 +0200

    read from db

--- src/Services/TrackerService.js
+++ src/Services/TrackerService.js
@@ -1,7 +1,24 @@
 const fs = require('fs')
+const {Facility} = require('../models')
 
 /* reading Health access.log */
 fs.readFile('../access.log', 'utf8', function(err, data) {
     if (err) throw err;
-    console.log(data);
+    //console.log(data);
 });
+
+async function getFacility(ip_address) {
+    try {
+        const facility = await Facility.findOne({
+            where: {
+              vpn_ip_address: ip_address
+            }
+        })
+
+        console.log(facility)
+    } catch (error) {
+        console.log("error: "+error)
+    }
+}
+
+getFacility('127.0.0.1')
