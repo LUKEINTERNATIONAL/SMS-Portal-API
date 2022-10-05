bd1c0417cac28f950cfe4f7c027adce827032bc6
dominickasanga <dominickasanga@gmail.com>
2022-10-05 14:26:22 +0200

    change messageId to messageID

--- src/Services/MessageService.js
+++ src/Services/MessageService.js
@@ -143,7 +143,7 @@ async function sendMessage() {
     let payload = {
       message: message[0][0]['body'],
       phone: message[0][0]['phone_pri'],
-      messageId: message[0][0].id,
+      messageID: message[0][0]['id'],
       ipAddress: getIpAddress()+":8186"
     }
     console.log(JSON.stringify(payload))
