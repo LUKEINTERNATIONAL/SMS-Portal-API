10178eb66fef0ac35374572b1fe2666152af293c
dominickasanga <dominickasanga@gmail.com>
2022-10-13 10:33:25 +0200

    finish GroupingConditions Service

--- src/Services/GroupingConditionService.js
+++ src/Services/GroupingConditionService.js
@@ -1,32 +1,41 @@
 const {Condition} = require('../models')
 const {GroupedCondition} = require('../models')
+const { sequelize } = require('../models')
 
 async function groupConditions() {
     const conditions = await getAllConditions()
-    
     if (conditions.length > 0) {
-        for (const condition of conditions) {
-            let generated_code_id = null
-            let condition_code = null
-            const _condition = await getCondition(condition.dataValues.name)
-            if (_condition) {
-                if (_condition.dataValues.name == condition.dataValues.name) {
-                    generated_code_id = _condition.dataValues.generated_code_id
-                    condition_code = _condition.dataValues.code
-
-                    if (condition_code != condition.dataValues.code) {
-                        const groupedCondition = await createGroupedCondition({
-                            generated_code_id: generated_code_id,
-                            code: condition.dataValues.code,
-                            name: condition.dataValues.name,
-                            active: 1
-                        })
-
-                        console.log("groupedCondition: "+groupedCondition)
+        for (let condition of conditions) {
+            const filteredConditions = filterConditions(conditions, condition.dataValues.name)
+            for (const filtredCondition of filteredConditions) {
+                condition = filtredCondition
+                let generated_code_id = null
+                let condition_code = null
+                const _condition = await getGroupedCondition(condition.dataValues.name)
+                if (_condition) {
+                    if (_condition.dataValues.name == condition.dataValues.name) {
+                        generated_code_id = _condition.dataValues.generated_code_id
+                        condition_code = _condition.dataValues.code
+    
+                        if (condition_code != condition.dataValues.code) {
+                            await createGroupedCondition({
+                                generated_code_id: generated_code_id,
+                                code: condition.dataValues.code,
+                                name: condition.dataValues.name,
+                                active: 1
+                            })
+    
+                        }
                     }
+                } else {
+                    const LastGeneratedCode =  parseInt( await getLastGeneratedCode()) +  parseInt(6)
+                    await createGroupedCondition({
+                        generated_code_id: LastGeneratedCode,
+                        code: condition.dataValues.code,
+                        name: condition.dataValues.name,
+                        active: 1
+                    })
                 }
-            } else {
-
             }
         }
     }
@@ -42,14 +51,13 @@ async function getAllConditions() {
     }
 }
 
-async function getCondition(name) {
+async function getGroupedCondition(name) {
     try {
         const condition = await GroupedCondition.findOne({
             where: {
                 name: name
             }
         })
-
         return condition
     } catch (error) {
         console.log(error)
@@ -67,4 +75,27 @@ async function createGroupedCondition(dataObject) {
     }
 }
 
-groupConditions()
+function filterConditions( object, name) {
+    const obj = object.filter( obj => {
+        return obj.name == name
+    })
+    return obj
+}
+
+async function getLastGeneratedCode() {
+    try {
+        const condition = await sequelize.query(`SELECT * FROM eidsr_sms_portal.GroupedConditions
+        where generated_code_id  
+           IS NOT NULL  
+               order by id 
+                   desc limit 1;`);
+   
+   
+       return condition[0][0]["generated_code_id"]
+    } catch (error) {
+        console.log(error)
+        return 594
+    }
+}
+
+module.exports = { groupConditions }
